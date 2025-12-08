8d7cc34c36a70cb76d87e88dd900a87fa5e6500c
nenw <khi@nenw.dev>
2025-12-08 16:39:30 +0000

    revert(pitch): test meyda
    This reverts commit 3afa40a90827d288ed4c5c80b161bc1f115732a7.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,4 @@
-import Meyda from 'meyda';
 import { CrepePitchDetector } from './utils/pitchDetection/CrepePitchDetector';
-import type { MeydaFeaturesObject } from 'meyda';
 
 let isLoaded = false;
 window.addEventListener('click', async () => {
@@ -12,39 +10,29 @@ window.addEventListener('click', async () => {
   const ctx = new AudioContext();
   const oscillator = ctx.createOscillator();
   const gain = ctx.createGain();
+  const movingWindow: [number, number][] = [];
+  const pitchDet = new CrepePitchDetector(ctx, ({ pitch, confidence }) => {
+    movingWindow.push([pitch, confidence]);
+    if (movingWindow.length > 40) {
+      movingWindow.unshift();
+    }
 
-  const audio = await fetch('/Hollowness-692637037.mp3').then(res => res.arrayBuffer());
-  const audioBuffer = await ctx.decodeAudioData(audio);
-  const source = ctx.createBufferSource();
-  source.buffer = audioBuffer;
-  source.start();
+    const { sum, cnt } = movingWindow.reduce(
+      ({ sum, cnt }, [pitch, confidence]) => ({
+        sum: sum + pitch * confidence,
+        cnt: cnt + confidence,
+      }),
+      { sum: 0, cnt: 0 }
+    );
 
-  const analyzer = Meyda.createMeydaAnalyzer({
-    audioContext: ctx,
-    source,
-    bufferSize: 1024,
-    featureExtractors: ['chroma'],
-    inputs: 2,
-    callback: (feature: MeydaFeaturesObject) => {
-      const sum = feature.chroma.reduce((prev, curr) => prev + curr);
-      const maxChroma = feature.chroma.reduce(
-        (prev, value, index) => (prev.value < value ? { value, index } : prev),
-        { value: 0, index: -1 }
-      );
-
-      if (sum === 0) {
-        return;
-      }
-
-      const snr = maxChroma.value / sum;
-      gain.gain.value = Math.min(1, Math.max(0, (snr * snr - 0.1 * 0.1) / (0.25 * 0.25))) * 0.6;
-      oscillator.frequency.value = 440 * Math.pow(2, (72 + maxChroma.index - 69) / 12);
-    },
+    oscillator.frequency.value = sum / cnt;
+    gain.gain.value = cnt / 70;
   });
 
   oscillator.frequency.value = 440;
   oscillator.connect(gain);
   oscillator.start();
-  analyzer.start();
   gain.connect(ctx.destination);
+  await pitchDet.init();
+  void pitchDet.start(await fetch('/puzzle-girl.mp3').then(res => res.arrayBuffer()));
 });
